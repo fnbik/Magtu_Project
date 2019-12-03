5cffb144100523bd142b82dac2eee3131faa2fea
Zhimovskij <zhimovskij@yandex.ru>
2019-12-04 00:48:37 +0500

    make parsing news posts from magtu portal

---
+++ Model/ParserHtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using System.Linq;
+
+namespace Model
+{
+    public class ParserHtml
+    {
+        public static async Task<News[]> ParseNews(string url)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var document = await BrowsingContext.New(config).OpenAsync(url);
+
+            List<News> news = new List<News>();
+
+            var titlePostList = document.QuerySelectorAll("h3[data-region-content=forum-post-core-subject]");
+            var authorPostList = document.QuerySelectorAll("h3[data-region-content=forum-post-core-subject]+address a");
+            var contentPostList = document.QuerySelectorAll(".post-content-container");
+
+            for (int i = 0; i < titlePostList.Length; i++)
+            {
+                news.Add(new News(titlePostList[i].TextContent, authorPostList[i].TextContent, "",
+                            new StringBuilder(contentPostList[i].TextContent)));
+            }
+
+            return news.ToArray();
+        }
+    }
+
+    public class News
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public string PublishDate { get; }
+        public StringBuilder Content { get; }
+
+        public News(string title, string author, string publishDate, StringBuilder content)
+        {
+            Title = title;
+            Author = author;
+            PublishDate = publishDate;
+            Content = content;
+        }
+    }
+}
--- UI/Views/MainWindow.xaml.cs
+++ UI/Views/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Model;
+using UI.Controls;
 
 namespace UI
 {
@@ -20,9 +22,33 @@ namespace UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        ScrollViewer _content { get; set; }
+
+        string _address = @"https://newlms.magtu.ru/";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _content = ContentMain;
+
+            PresentNews();
+        }
+
+        public void PresentNews()
+        {
+
+            var task = Task.Run(() => ParserHtml.ParseNews(_address));
+            task.Wait();
+
+            News[] news = task.Result;
+            StackPanel newsPanel = new StackPanel() {Orientation=Orientation.Vertical};
+            for (int i = 0; i < news.Length; i++)
+            {
+                NewsPost post = new NewsPost(news[i].Title, news[i].Author, news[i].Content.ToString());
+                newsPanel.Children.Add(post);
+            }
+            _content.Content = newsPanel;
         }
 
         private void DragPlace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
